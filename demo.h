cc4a3661a0cf4323d0ef047091bbdf4d830285b1
Luceropalomino <136868686+Luceropalomino@users.noreply.github.com>
2023-08-12 10:28:15 -0400

    git add .

--- demo.h
+++ demo.h
@@ -6,3 +6,22 @@ void DemoBinaryTree();
 void DemoHash();
 
 #endif // __DEMO_H__
+
+#include <iostream> // cout
+#include "demo.h"
+#include "heap.h"
+using namespace std;
+
+int main() {
+    DemoArray();
+    DemoBinaryTree();
+    DemoHash();
+    CHeap<XTraitIntIntAsc> h;
+    h.insert(5, "Hello");
+    h.insert(10, "World");
+    h.insert(2, "!");
+    cout << h[1] << endl; // Prints "5"
+    cout << h.pop().getData() << endl; // Prints "2"
+    cout << h.size() << endl; // Prints "2"
+    return 0;
+}
