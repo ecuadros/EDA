af03d0641ce3a65c2469f20948508feba33cd05a
Luceropalomino <136868686+Luceropalomino@users.noreply.github.com>
2023-08-16 10:51:02 -0400

    git add .

---
+++ EDA-11-Templates/array.h
@@ -0,0 +1,67 @@
+#ifndef __ARRAY_H__
+#define __ARRAY_H__
+
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class CArray {
+private:
+    typedef T value_type;
+    value_type *m_pVect = nullptr;
+    size_t m_vcount = 0, m_vmax = 0;
+    string m_name = "Empty";
+
+public:
+    CArray() : m_name("Empty") {}
+    CArray(string name) : m_name(name) {}
+    ~CArray() {
+        cout << "Destroying " << m_name << "..." << endl;
+        delete[] m_pVect;
+        m_pVect = nullptr;
+        m_vcount = 0;
+        m_vmax = 0;
+    }
+    void insert(value_type val) {
+        if (m_vcount == m_vmax) // Array is already full?
+            resize();
+        m_pVect[m_vcount++] = val;
+        cout << "Val=" << val << " inserted, m_vcount=" << m_vcount << " m_vmax=" << m_vmax << endl;
+    }
+    void resize();
+    void print(ostream &os) {
+        os << m_vcount << " " << m_vmax << endl;
+        for (size_t i = 0; i < m_vcount; ++i)
+            os << m_pVect[i] << endl;
+    }
+    size_t size() {
+        return m_vcount;
+    }
+    value_type &operator[](size_t pos) {
+        return m_pVect[pos];
+    }
+};
+
+template <typename T>
+void CArray<T>::resize() {
+    value_type *pTemp = new value_type[m_vmax + 10];
+    for (size_t i = 0; i < m_vcount; ++i)
+        pTemp[i] = m_pVect[i];
+    delete[] m_pVect;
+    m_pVect = pTemp;
+    m_vmax += 10;
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, CArray<T> &obj) {
+    obj.print(os);
+    return os;
+}
+
+template <typename T>
+istream &operator>>(istream &is, CArray<T> &obj) {
+    // TODO: Implementar la sobrecarga del operador de extracción (>>)
+    return is;
+}
+
+#endif // __ARRAY_H__
