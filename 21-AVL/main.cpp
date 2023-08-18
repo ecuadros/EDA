7eebb996e0adba2f2b2d200fd3a0421a3ffd2f2e
Luceropalomino <136868686+Luceropalomino@users.noreply.github.com>
2023-08-18 13:07:08 -0500

    git add .

--- 21-AVL/main.cpp
+++ 21-AVL/main.cpp
@@ -1,27 +1,12 @@
-#include <iostream>
-#include "demo.h"
-// #include "array.h"
-
-using namespace std;
-
-// g++ -pthread -std=c++17   -o main main.cpp demo.cpp
-// g++ -pthread -std=c++20   -o main main.cpp demo.cpp
-// g++ -pthread -std=c++2b   -o main main.cpp demo.cpp
-// g++ -pthread -std=gnu++2b -o main main.cpp demo.cpp
 int main()
-{
-    // DemoParameters();
-    // DemoArray();
-    // DemoIterators();
     // DemoReverseIterators();
     // DemoDynamicMatrixes();
 
     DemoHeap();
     // DemoBinaryTree();
+    //DemoHeap();
+    //DemoBinaryTree();
+    DemoAVLTree();
     // DemoHash();
 
     // DemoPreandPostIncrement();
-    // DemoSmartPointers();
-    
-    return 0;
-}
