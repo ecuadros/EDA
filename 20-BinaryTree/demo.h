7116e031aacfad0000d037c76c9479d61efd06d0
Luceropalomino <136868686+Luceropalomino@users.noreply.github.com>
2023-08-18 12:41:07 -0500

    git add .

---
+++ 20-BinaryTree/demo.h
@@ -0,0 +1,20 @@
+#ifndef __DEMO_H__
+#define __DEMO_H__
+
+void DemoBasicPointers();
+void DemoSmartPointers();
+void DemoDynamicMatrixes();
+void DemoPreandPostIncrement();
+
+void DemoArray();
+void DemoIterators();
+void DemoReverseIterators();
+void DemoHeap();
+
+void DemoBinaryTree();
+void DemoHash();
+
+
+
+
+#endif // __DEMO_H__
